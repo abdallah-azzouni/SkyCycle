0c6c0affaa889b03aa4380172e70e6945caf972b
a.azzouni <xara25az@gmail.com>
2025-10-19 11:42:41 +0300

    Add common.py for JSON data handling

---
+++ src/common.py
@@ -0,0 +1,27 @@
+import json
+import os
+
+data_file = "data.json"
+template = {"location": None, "active_profile": None}
+
+
+def load() -> dict[str, None]:
+    if not os.path.exists(data_file) or os.stat(data_file).st_size == 0:
+        with open(data_file, "w", encoding="utf-8") as f:
+            json.dump(template, f, indent=4)
+
+    with open(data_file, "r+", encoding="utf-8") as f:
+        try:
+            data = json.load(f)
+        except json.JSONDecodeError:
+            data = template
+            f.seek(0)
+            json.dump(data, f, indent=4)
+            f.truncate()
+
+    return data
+
+
+def write(data):
+    with open(data_file, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
